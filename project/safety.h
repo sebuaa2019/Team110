c7b2d1988f1e8bae870789be31260860380ecf13
C01DZin <732345308@qq.com>
2019-05-18 20:31:13 +0800

    完善报警算法和参数修改

--- project/safety.h
+++ project/safety.h
@@ -9,7 +9,7 @@ void turn_on_invade_sensor();
 void turn_off_invade_sensor();
 
 double get_smoke_concen();
-
+void cancel_invade_by_owner();/* 房主回家，在指定时间内输入密码，取消入侵检测*/
 int onMovement();
 int onDoor();
 int detect_smoke();
