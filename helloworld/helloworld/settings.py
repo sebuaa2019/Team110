9d5f5242affbfbcaac5eb80849179797dda69b26
towelieWizard <978149308@buaa.edu.cn>
2019-05-19 15:00:48 +0800

    新增给管理员发邮件功能

--- helloworld/helloworld/settings.py
+++ helloworld/helloworld/settings.py
@@ -128,3 +128,12 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.qq.com'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = '978149308@qq.com'
+EMAIL_HOST_PASSWORD = 'bktsalunltahbdci'
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
+EMAIL_FROM ='978149308@qq.com'
