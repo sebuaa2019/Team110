d6583b7cea096b9bc4eca1ad7c02e7917a207923
C01DZin <732345308@qq.com>
2019-05-27 15:56:57 +0800

    fix

--- spider/spider.py
+++ spider/spider.py
@@ -8,13 +8,16 @@ while(1):
     time.sleep(1)
     url = "http://114.115.255.141:8001/status/"
     header = {
-        "Cookie": "csrftoken=ewmmNLpczV1r8D6dTrrf5kWUTdOFGdecVfGsc5RD6XzC0XbrBc015mnFSstWBM8M; sessionid=fixf9j2cx3bb52k68hona6zlja2oadxy"}
+        "Cookie": "csrftoken=ewmmNLpczV1r8D6dTrrf5kWUTdOFGdecVfGsc5RD6XzC0XbrBc015mnFSstWBM8M; sessionid=fixf9j2cx3bb52k68hona6zlja2oadxy"
+    }
+    # cookie 需要自己查看自己登录信息
     response = requests.get(url, headers=header)
     response.encoding = 'utf-8'
     html = response.text
     status = re.findall(r'value="系统状态：关" (.*?)>系统状态：关', html)
     print(status)
     f = open(r"C:\Users\周萌\Desktop\share\a.out", "w")
+    # 共享文件位置
     if status[0] == "checked":
         f.write("close")
     else:
